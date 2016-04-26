c28a82d7e39a5d6e1bd4d57f84bc1e540bb446f5
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-04-25 22:30:13 -0700

    Added a map with markers.

--- index.js
+++ index.js
@@ -3,8 +3,53 @@ var addTableBody = function(){
 	response.complete(addBodyCallback);
 }
 
+function initMap(){
+	var myLatLng = {lat: 0, lng: 0};
+
+	var map = new google.maps.Map(document.getElementById('map'),{
+		zoom: 10,
+		center: myLatLng
+	});
+
+	// create an empty string to hold instances of all marker objects.
+	var marker = [];
+	// all lat and lng values will be averaged.  the map center will be set to the average lat and lng.
+	var getAverage = [];
+
+	for (i in objs){
+		// create a location object with the lat and lng equal to that of spot.
+		var lat = objs[i].location[0];
+		var lng = objs[i].location[1];
+		myLatLng = {lat: lat, lng: lng};
+		
+		getAverage.push(myLatLng);
+
+		// populate the marker array.
+		marker[i] = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: objs[i].name
+		});
+	}
+
+	// calculate the average location.
+	var latSum = 0;
+	var lngSum = 0;
+	for (i in getAverage){
+		latSum += getAverage[i].lat;
+		lngSum += getAverage[i].lng;
+	}
+	var latAvg = latSum / (getAverage.length);
+	var lngAvg = lngSum / (getAverage.length);
+	map.setCenter(new google.maps.LatLng(latAvg, lngAvg));
+}
+
+
+var objs = {};
+
 var addBodyCallback = function(response){
-	var objs = response.responseJSON;
+
+	objs = response.responseJSON;
 	var $tbody = $("<tbody></tbody>");
 
 	// iterate over every row object and create an html row with the contents
@@ -20,6 +65,7 @@ var addBodyCallback = function(response){
 		// add the link the location on google maps.
 		var $a = $('<a class="btn btn-info mrs">Open in Google Maps</a>');
 		var googleMapsURL = "https://www.google.com/maps?q=";
+		$a.attr("target", "_blank");
 		googleMapsURL += obj.location[0] + "," + obj.location[1];
 		// add the link to the anchor tag.
 		$a.attr("href", googleMapsURL);
@@ -37,6 +83,8 @@ var addBodyCallback = function(response){
 	});
 
 	$("thead").after($tbody);
+
+	$('head').append('<script async defer type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGlrqrO_evftmLh-qieGpY9gKCKCYPBV0&callback=initMap"></script>');
 }
 
 $(addTableBody);
